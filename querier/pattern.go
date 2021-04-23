013a7b1a55b4404f6426c388728e573acf8e49f2
Serg Kotovsky <info@geekserg.ru>
2021-04-23 20:58:40 +0300

    pattern

--- querier/pattern.go
+++ querier/pattern.go
@@ -1,5 +1,5 @@
 package querier
 
 var (
-	queryPatterns []string = []string{"select from", "select from where"}
+	QueryPatterns []string = []string{`^select\b.+\bfrom\b.+\bwhere\b.+`, `^select\b.+\bfrom\b.+`}
 )
